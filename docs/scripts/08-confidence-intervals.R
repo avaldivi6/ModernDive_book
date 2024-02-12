be4f533e00f7618d9fa57af7e8473f4e94615254
Chester Ismay <chester.ismay@gmail.com>
2024-02-12 09:51:16 -0800

    Fix typos

--- docs/scripts/08-confidence-intervals.R
+++ docs/scripts/08-confidence-intervals.R
@@ -6,42 +6,144 @@ library(infer)
 
 
 
+## ----echo=-1------------------------------------------------------------------
+almonds_bowl <- read_rds("rds/almonds_bowl.rds")
+almonds_bowl |> 
+  summarize(population_mean = mean(weight), 
+            population_sd = sd(weight))
 
 
-## ----message=FALSE------------------------------------------------------------
-library(tidyverse)
-library(moderndive)
-library(infer)
+## ----echo=FALSE---------------------------------------------------------------
+almonds_bowl <- read_rds("rds/almonds_bowl.rds")
+if (!file.exists("rds/almonds_sample_100.rds")) {
+  set.seed(20)
+  almonds_sample_100 <- almonds_bowl |>
+    rep_slice_sample(n = 100, replace = TRUE, reps = 1)
+  write_rds(almonds_sample_100, "rds/almonds_sample_100.rds")
+} else {
+  almonds_sample_100 <- read_rds("rds/almonds_sample_100.rds")
+}
+almonds_sample_100
 
 
+## -----------------------------------------------------------------------------
+almonds_sample_100 |> 
+  summarize(mean_weight = mean(weight), 
+            sd_weight = sd(weight), 
+            sample_size = n())
 
 
-
-
-
-
-## ----percentile-method, echo=FALSE, message=FALSE, fig.cap="(ref:perc-method)", fig.height=3.4----
-ggplot(virtual_resampled_means, aes(x = mean_weight)) +
-  geom_histogram(binwidth = 1, color = "white", boundary = 1988) +
-  labs(x = "Resample sample mean") +
-  scale_x_continuous(breaks = seq(1988, 2006, 2)) +
-  geom_vline(xintercept = percentile_ci[[1, 1]], size = 1) +
-  geom_vline(xintercept = percentile_ci[[1, 2]], size = 1)
 
 
 
 
 ## -----------------------------------------------------------------------------
-virtual_resampled_means %>% 
-  summarize(SE = sd(mean_weight))
+pnorm(1)
+
+
+## ----normal-curve-shaded-1----------------------------------------------------
+ ggplot(NULL, aes(c(-4,4))) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey50", xlim = c(-4, 1)) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey100", xlim = c(1, 4)) +
+  labs(x = "z", y = "") +
+  scale_y_continuous(breaks = NULL) +
+  scale_x_continuous(breaks = 1)
+
+
+## ----normal-curve-shaded-2----------------------------------------------------
+ ggplot(NULL, aes(c(-4,4))) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey100", xlim = c(-4, -1)) +
+    geom_area(stat = "function", fun = dnorm, fill = "grey80", xlim = c(-1, 1)) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey100", xlim = c(1, 4)) +
+  labs(x = "z", y = "") +
+  scale_y_continuous(breaks = NULL) +
+  scale_x_continuous(breaks = c(-1,1))
+
+
+## -----------------------------------------------------------------------------
+pnorm(1) - pnorm(-1)
+
+
+## -----------------------------------------------------------------------------
+pnorm(2) - pnorm(-2)
+
+
+## -----------------------------------------------------------------------------
+qnorm(0.84)
+
+
+## ----normal-curve-shaded-3----------------------------------------------------
+ ggplot(NULL, aes(c(-4,4))) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey100", xlim = c(-4, -2)) +
+    geom_area(stat = "function", fun = dnorm, fill = "grey80", xlim = c(-2, 2)) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey100", xlim = c(2, 4)) +
+  labs(x = "z", y = "") +
+  scale_y_continuous(breaks = NULL) +
+  scale_x_continuous(breaks = NULL) + 
+  annotate(geom="text", x=2, y=-0.01, label="q",
+              color="blue")
+
+
+## -----------------------------------------------------------------------------
+q <- qnorm(0.975)
+q
+
+
+## -----------------------------------------------------------------------------
+qnorm(0.99)
 
 
 
 
-## ----eval=FALSE---------------------------------------------------------------
-## standard_error_ci <- bootstrap_distribution %>%
-##   get_ci(type = "se", point_estimate = x_bar)
-## standard_error_ci
+## -----------------------------------------------------------------------------
+almonds_bowl |> 
+  summarize(sigma = sd(weight))
+almonds_sample_100 |> 
+  summarize(mean_weight = mean(weight),
+            sample_size = n())
+
+
+## -----------------------------------------------------------------------------
+xbar <- 3.682 
+se_xbar <- 0.392/sqrt(100)
+lower_bound <- xbar - 1.96 *  se_xbar
+upper_bound <- xbar + 1.96 *  se_xbar
+c(lower_bound, upper_bound)
+
+
+
+
+## -----------------------------------------------------------------------------
+almonds_sample_100 |> 
+  summarize(mean_weight = mean(weight),
+            sd_weight = sd(weight),
+            sample_size = n())
+
+
+## -----------------------------------------------------------------------------
+qt(0.975, df = 100 - 1)
+
+
+## -----------------------------------------------------------------------------
+xbar <- 3.682 
+se_xbar <- 0.362/sqrt(100)
+lower_bound <- xbar - 1.98 *  se_xbar
+upper_bound <- xbar + 1.98 *  se_xbar
+c(lower_bound, upper_bound)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -163,101 +265,6 @@ standard_error_ci
 
 
 ## -----------------------------------------------------------------------------
-bowl %>% 
-  summarize(p_red = mean(color == "red"))
-
-
-
-## -----------------------------------------------------------------------------
-bowl_sample_1
-
-
-
-
-## ----eval=FALSE---------------------------------------------------------------
-## bowl_sample_1 %>%
-##   specify(response = color)
-
-
-## -----------------------------------------------------------------------------
-bowl_sample_1 %>% 
-  specify(response = color, success = "red")
-
-
-## ----eval=FALSE---------------------------------------------------------------
-## bowl_sample_1 %>%
-##   specify(response = color, success = "red") %>%
-##   generate(reps = 1000, type = "bootstrap")
-
-
-
-## ----eval=FALSE---------------------------------------------------------------
-## sample_1_bootstrap <- bowl_sample_1 %>%
-##   specify(response = color, success = "red") %>%
-##   generate(reps = 1000, type = "bootstrap") %>%
-##   calculate(stat = "prop")
-## sample_1_bootstrap
-
-
-
-
-## -----------------------------------------------------------------------------
-percentile_ci_1 <- sample_1_bootstrap %>% 
-  get_confidence_interval(level = 0.95, type = "percentile")
-percentile_ci_1
-
-
-## ----eval=FALSE---------------------------------------------------------------
-## sample_1_bootstrap %>%
-##   visualize(bins = 15) +
-##   shade_confidence_interval(endpoints = percentile_ci_1) +
-##   geom_vline(xintercept = 0.42, linetype = "dashed")
-
-
-
-
-## -----------------------------------------------------------------------------
-bowl_sample_2 <- bowl %>% rep_sample_n(size = 50)
-bowl_sample_2
-
-
-## ----eval=FALSE---------------------------------------------------------------
-## sample_2_bootstrap <- bowl_sample_2 %>%
-##   specify(response = color,
-##           success = "red") %>%
-##   generate(reps = 1000,
-##            type = "bootstrap") %>%
-##   calculate(stat = "prop")
-## sample_2_bootstrap
-
-
-
-
-## -----------------------------------------------------------------------------
-percentile_ci_2 <- sample_2_bootstrap %>% 
-  get_confidence_interval(level = 0.95, type = "percentile")
-percentile_ci_2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## -----------------------------------------------------------------------------
 mythbusters_yawn
 
 
@@ -340,100 +347,4 @@ obs_diff_in_props
 myth_ci_se <- bootstrap_distribution_yawning %>% 
   get_confidence_interval(type = "se", point_estimate = obs_diff_in_props)
 myth_ci_se
-
-
-
-
-
-
-## ----echo=FALSE---------------------------------------------------------------
-set.seed(76)
-
-
-## ----sampling-distribution-part-deux, fig.show="hold", fig.cap="Previously seen sampling distribution of sample proportion red for $n = 1000$.", fig.height=2----
-# Take 1000 virtual samples of size 50 from the bowl:
-virtual_samples <- bowl %>% 
-  rep_sample_n(size = 50, reps = 1000)
-# Compute the sampling distribution of 1000 values of p-hat
-sampling_distribution <- virtual_samples %>% 
-  group_by(replicate) %>% 
-  summarize(red = sum(color == "red")) %>% 
-  mutate(prop_red = red / 50)
-# Visualize sampling distribution of p-hat
-ggplot(sampling_distribution, aes(x = prop_red)) +
-  geom_histogram(binwidth = 0.05, boundary = 0.4, color = "white") +
-  labs(x = "Proportion of 50 balls that were red", 
-       title = "Sampling distribution")
-
-
-## -----------------------------------------------------------------------------
-sampling_distribution %>% summarize(se = sd(prop_red))
-
-
-
-## ----echo=FALSE---------------------------------------------------------------
-set.seed(76)
-
-
-## ----eval=FALSE---------------------------------------------------------------
-## bootstrap_distribution <- bowl_sample_1 %>%
-##   specify(response = color, success = "red") %>%
-##   generate(reps = 1000, type = "bootstrap") %>%
-##   calculate(stat = "prop")
-
-
-
-
-
-
-## -----------------------------------------------------------------------------
-bootstrap_distribution %>% summarize(se = sd(stat))
-
-
-
-
-
-
-
-
-
-## ----message=FALSE------------------------------------------------------------
-conf_ints <- tactile_prop_red %>% 
-  rename(p_hat = prop_red) %>% 
-  mutate(
-    n = 50,
-    SE = sqrt(p_hat * (1 - p_hat) / n),
-    MoE = 1.96 * SE,
-    lower_ci = p_hat - MoE,
-    upper_ci = p_hat + MoE
-  )
-
-
-
-
-
-
-
-
-## ----eval=FALSE---------------------------------------------------------------
-## # First: Take 100 virtual samples of n=50 balls
-## virtual_samples <- bowl %>%
-##   rep_sample_n(size = 50, reps = 100)
-## 
-## # Second: For each virtual sample compute the proportion red
-## virtual_prop_red <- virtual_samples %>%
-##   group_by(replicate) %>%
-##   summarize(red = sum(color == "red")) %>%
-##   mutate(prop_red = red / 50)
-## 
-## # Third: Compute the 95% confidence interval as before
-## virtual_prop_red <- virtual_prop_red %>%
-##   rename(p_hat = prop_red) %>%
-##   mutate(
-##     n = 50,
-##     SE = sqrt(p_hat*(1-p_hat)/n),
-##     MoE = 1.96 * SE,
-##     lower_ci = p_hat - MoE,
-##     upper_ci = p_hat + MoE
-##   )
 
